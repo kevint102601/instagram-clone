5760a594399a324083f8e43e5a7c46ee537850df
Kevin Tran <kevint102601@gmail.com>
2024-03-22 20:04:35 -0700

    Starting on Homepage

---
+++ src/components/AuthForm/Sidebar/Sidebar.jsx
@@ -0,0 +1,37 @@
+import { Box, Flex, Link } from '@chakra-ui/react'
+import { Link as RouterLink} from 'react-router-dom'
+import { InstagramLogo, InstagramMobileLogo } from '../../../assets/constants'
+
+const Sidebar = () => {
+  return <Box
+  height={"100vh"}
+  borderRight={"1px solid"}
+  borderColor={"WhiteAlpha.300"}
+  py={8}
+  position={"sticky"}
+  top={0}
+  left={0}
+  px={{base:2,md:4}}
+  >
+    <Flex direction={"column"} gap={10} w="full" height={"full"}> 
+        <Link to={"/"} as={RouterLink} pl={2} display={{base:"none",md:"block"}} cursor="pointer" >
+            <InstagramLogo/>
+
+        </Link>
+        <Link to={"/"} as={RouterLink} p={2} display={{base:"block",md:"none"}}
+            borderRaduis={6}
+            _hover={{
+                bg:"whiteAlpha.200"
+        }}
+        w={10} 
+        cursor="pointer" >
+            <InstagramMobileLogo/>
+        </Link>
+
+    </Flex>
+
+  </Box>
+  
+}
+
+export default Sidebar
